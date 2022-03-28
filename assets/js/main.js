27c3498f643f6ad75760b914c9a1e50106bccb19
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-28 15:45:17 +0200

    milestone 1 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const element_container = document.querySelector('.container');
 
-for (i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     let element_square_number = `<div class="square bg_aqua">${i}</div>`;
     element_container.insertAdjacentHTML('beforeend', element_square_number);
 }
--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,6 +1,27 @@
+//prendo container
 const element_container = document.querySelector('.container');
 
-for (i = 1; i <= 100; i++) {
-    let element_square_number = `<div class="square bg_aqua">${i}</div>`;
-    element_container.insertAdjacentHTML('beforeend', element_square_number);
+
+//creo constanti per fizz e buzz
+const fizz = 'Fizz '
+const buzz = 'Buzz'
+
+//dichiaro variabile quadrato + text
+let text, element_square;
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        text = fizz + buzz
+
+    } else if (i % 5 == 0) {
+        text = buzz
+
+    } else if (i % 3 == 0) {
+        text = fizz
+
+    } else {
+        text = i
+    }
+    console.log(text);
 }
