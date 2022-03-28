3cd90aaecac20911c859005ef6e3d143702d340b
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-28 15:56:58 +0200

    add milestone 2 complete ( è orribile)

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -7,21 +7,35 @@ const fizz = 'Fizz '
 const buzz = 'Buzz'
 
 //dichiaro variabile quadrato + text
-let text, element_square;
+let text, element_div;
 
 for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
+
         text = fizz + buzz
+        element_div = document.createElement('div')
+        element_div.insertAdjacentHTML("beforeend", text)
 
     } else if (i % 5 == 0) {
+
         text = buzz
+        element_div = document.createElement('div')
+        element_div.insertAdjacentHTML("beforeend", text)
 
     } else if (i % 3 == 0) {
+
         text = fizz
+        element_div = document.createElement('div')
+        element_div.insertAdjacentHTML("beforeend", text)
 
     } else {
+
         text = i
+        element_div = document.createElement('div')
+        element_div.insertAdjacentHTML("beforeend", text)
+
     }
-    console.log(text);
+
+    element_container.append(element_div)
 }
